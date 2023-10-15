dbe35841eaebdf7789fc39162212843c3b92d42e
Pasan Abeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-10-15 07:29:17 +0530

    Add Auth0

--- src/App.js
+++ src/App.js
@@ -7,13 +7,24 @@ import PostForm from './components/PostForm';
 import UserDashboard from './components/UserDashboard';
 import AllRequests from './components/AllRequests';
 import AllRides from './components/AllRides';
+import LoginButton from './components/auth/LoginButton';
+import LogoutButton from './components/auth/LogoutButton';
+import ProfilePage from './components/auth/ProfilePage';
+import ProfileButton from './components/auth/ProfileButton';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <LoginButton/>
+        <LogoutButton/>
+        <ProfileButton/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProtectedRoute />}>
+            <Route index element={<ProfilePage />} />
+          </Route>
           <Route path="/rides" element={<AllRides />} />
           <Route path="/rides/:id" element={<RideDetails />} />
           <Route path="/rides/:id/create-request" element={<CreateRequest />} />
