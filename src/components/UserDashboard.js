2f8fc926e6d0f1027eba55b822e796aa8a2c7cf4
Pasan Abeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-10-16 17:16:10 +0530

    userdashboard styling done

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -26,35 +26,36 @@ class UserDashboard extends Component {
             <div className="">
                 {/* User Dashboard */}
                 <div className="container mx-auto pt-10">
-                    <h1 className="text-4xl font-bold text-center mb-8">Welcome to Your Dashboard, {isAuthenticated ? user.name : 'Guest'}</h1>
+                    <h1 className="text-4xl font-bold text-center">Welcome to Your Dashboard, {isAuthenticated ? user.name : 'Guest'}</h1>
                     
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-20">
                         <div className="bg-white rounded-lg shadow-md p-6">
                             <h2 className="text-2xl font-bold mb-4 text-gray-800">Profile Information</h2>
                             <div className="mb-4">
                                 <p className="text-lg text-gray-800">Name:</p>
-                                <p className="text-gray-600">{isAuthenticated ? user.name : 'Guest'}</p>
+                                <p className="text-gray-600 text-xl font-medium">{isAuthenticated ? user.name : 'Guest'}</p>
                             </div>
                             <div className="mb-4">
                                 <p className="text-lg text-gray-800">Email:</p>
-                                <p className="text-gray-600">{isAuthenticated ? user.email : 'guest@example.com'}</p>
+                                <p className="text-gray-600 text-xl font-medium">{isAuthenticated ? user.email : 'guest@example.com'}</p>
                             </div>
                         </div>
+
                         <div className="bg-white rounded-lg shadow-md p-6">
-                            <h2 className="text-2xl font-bold mb-4 text-gray-800">My Requests</h2>
-                            <div className="mb-4">
+                            <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center">My Requests</h2>
+                            <div className="mb-8 text-center">
                                 <p className="text-lg text-gray-800">You have {requestCount} active Requests.</p>
                             </div>
-                            <a href="/all-requests" className="block w-full text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
+                            <a href="/all-requests" className="mt-16 block w-full text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
                                 View Requests
                             </a>
                         </div>
                         <div className="bg-white rounded-lg shadow-md p-6">
-                            <h2 className="text-2xl font-bold mb-4 text-gray-800">Available Rides</h2>
-                            <div className="mb-4">
+                            <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center">Available Rides</h2>
+                            <div className="mb-8 text-center">
                                 <p className="text-lg text-gray-800">Find all available rides here.</p>
                             </div>
-                            <a href="/rides" className="block w-full text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
+                            <a href="/rides" className="mt-16 block w-full text-center bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md">
                                 View Available Rides
                             </a>
                         </div>
